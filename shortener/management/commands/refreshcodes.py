a73ace0d3719c3000b9fe592ea8e21dd1103c257
Vitaliy <23neyron@gmail.com>
2017-01-19 03:37:08 +0300

    add custom django managment commands

---
+++ shortener/management/commands/refreshcodes.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+from shortener.models import ShortURL
+
+class Command(BaseCommand):
+    help = "Refresh all ShortURL shortcodes"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--items", type=int)
+
+    def handle(self, *args, **options):
+        return ShortURL.objects.refresh_shortcodes(items=options["items"])
--- shortener/models.py
+++ shortener/models.py
@@ -1,36 +1,41 @@
-from django.db import models
 from .utils import code_generator, create_shortcode
+from django.db import models
 
 
-class ShortURLManager(models.Model):
+class ShortURLManager(models.Manager):
     def all(self, *args, **kwargs):
         qs_main = super(ShortURLManager, self).all(*args, **kwargs)
-        qs = qs_main.filter(active=False)
+        qs = qs_main.filter(active=True)
         return qs
 
-    def refresh_shortcodes(self):
+    def refresh_shortcodes(self, items=None):
         qs = ShortURL.objects.filter(id__gte=1)
+        if items is not None and isinstance(items, int):
+            qs = qs.order_by("-id")[:items]
         new_codes = 0
         for q in qs:
             q.shortcode = create_shortcode(q)
-            print(q.shortcode)
+            print(q.id)
             q.save()
-            ++new_codes
+            new_codes += 1
         return "New codes made: {i}".format(i=new_codes)
 
 class ShortURL(models.Model):
     url       = models.CharField(max_length=230,)
-    shortcode = models.CharField(max_length=15, unique=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
     update    = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     active    = models.BooleanField(default=True)
-    objects = ShortURLManager()
+    objects   = ShortURLManager()
     # some_random = ShortURLManager()
 
     def save(self, *args, **kwargs):
         if self.shortcode is None or self.shortcode == "":
-            self.shortcode = code_generator()
+            self.shortcode = create_shortcode(self)
         super(ShortURL, self).save(*args, **kwargs)
+
+    # class Meta:
+    #     ordering = "-id"
 
     def __str__(self):
         return str(self.url)
